33c1954eeb14208e79b55802766843f6dac6d54c
ViktorBed <vbednarski@ukr.net>
2023-07-10 00:06:42 +0300

    commit

---
+++ js/dz1/script.js
@@ -0,0 +1,36 @@
+let arr = ['hello', 'owu', 'com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false]
+console.log(arr [0])
+console.log(arr [1])
+console.log(arr [2])
+console.log(arr [3])
+console.log(arr [4])
+console.log(arr [5])
+console.log(arr [6])
+console.log(arr [7])
+console.log(arr [8])
+console.log(arr [9])
+console.log(arr [10])
+console.log(arr [11])
+console.log(arr [12])
+
+
+let person = {
+    firstName: 'Viktor',
+    middleName: 'Broflovski',
+    lastName: 'Yaroslavovish',
+}
+
+console.log(person.firstName);
+console.log(person.middleName);
+console.log(person.lastName);
+
+
+let a = 100;
+let b = '100';
+let c = true;
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
+
+
+
