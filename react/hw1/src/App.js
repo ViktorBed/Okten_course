bdc99ddf9f3e1e79bebae10d850ffb21fb47a511
= <vbednarskii@gmail.com>
2023-08-29 20:42:36 +0300

    com

---
+++ react/hw1/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Simpsons from "./Components/Simpsons";
+import Users from "./Components/Users";
+
+// Взяти масив usersList.
+//     Створити компонент,котрий характеризує юзера.
+//     Створити компонент,котрий характеризує підоб'єкт юзера address. Використати в компоненті юзера.
+// Створити компонент,котрий характеризує підоб'єкт адреси юзера geo. Використати в компоненті адреси.
+// Вкладеність компонентів : App->Users->User->Address->Geo
+// Вивести всі об'єкти з масиву
+function App() {
+  return (
+   <div>
+     <Simpsons/>
+       <Users/>
+   </div>
+  );
+}
+
+export default App;
